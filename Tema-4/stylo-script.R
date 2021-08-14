4c5d7d02186025a51990ddff1e3c542128c39be9
Laura HL <40395141+lamusadecima@users.noreply.github.com>
2021-08-14 13:54:07 +0200

    Add files via upload

---
+++ Tema-4/stylo-script.R
@@ -0,0 +1,26 @@
+#################################################
+##                                             ##
+##         ESTRATEGIAS DIGITALES PARA          ##
+##        LA INVESTIGACIÓN EN LITERATURA       ##
+##                                             ##
+##       Tema 4: La Estilometría               ##
+##                                             ##
+#################################################
+
+
+# Primero establecemos el directorio de trabajo
+# Si tienes un Mac y has guardado la carpeta "novela" en el escritorio:
+setwd("Desktop/novela")
+# Si tienes Windows y has guardado la carpeta en C:
+setwd("C:/novela")
+
+# Si las líneas de arriba te dan error, otra opción para establecer el 
+#directorio de trabajo es hacer clic en Session > Set working directory
+
+# A continuación, llamamos al paquete stylo
+
+library(stylo)
+
+# Para funciones básicas de Estilometría
+
+stylo()
